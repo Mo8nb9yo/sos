9cf85783b1d1eced3fd0aec6a61bc6feabee2b05
Mo8nb9yo <hatsemarian@gmail.com>
2025-09-23 19:50:06 +0000

    sos

---
+++ kiku1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+long long sumEvenlnRange(long long a, long long b) {
+if (a>b) swap(a,b);
+long long first = (a % 2 !=0) ? (a+1) : a;
+long long last =(b % 2 != 0) ? (b - 1) : b;
+if (first > last) return 0LL;
+long long n = ((last - first)/2) + 1;
+return n*(first + last)/2;//safe with long long
+}
+int main() {
+long long a, b;
+count <<"numbers two!:";
+if (!(cin >>a>> b)) return 0;
+count <<"suma two pair:" << sumEvenlnRange(a, b)<< "\n";
+return 0;
+}
---
+++ kuki.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+ int a, b;
+ cout <<two numbers please: ";
+ cin >>a >>b;
+
+int sum=0
+for (int i=a; i<=b; i++){
+	if(i % 2==0){//cheking
+	sum += i;
+	}
+}
+
+cout <<"suma parni[" 
+<<a <<";" <<b <<"] = " sum << endl;
+return 0;
+)
